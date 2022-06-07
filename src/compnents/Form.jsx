598e5e57635f647b3508f7979f0fce4582f14653
Lukasgit config --global user.email lukas@gmail.comgit add .git init -m init <you@example.com>
2022-06-07 07:54:04 +0300

    second page done.

--- src/compnents/Form.jsx
+++ src/compnents/Form.jsx
@@ -1,11 +1,31 @@
 import InputField from './UI/InputField';
 import TextArea from './UI/TextArea';
 import Sbutton from './UI/Sbutton';
+import { useState } from 'react';
 
 function Form() {
-  function logEvent(event) {
+  const [createdAtt, setCreatedAt] = useState('');
+
+  async function postForm(data) {
+    try {
+      const response = await fetch('https://reqres.in/api/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      const dataInJs = await response.json();
+      console.log('dataInJs ===', dataInJs);
+      setCreatedAt(dataInJs.createdAt);
+    } catch (error) {
+      console.log('error ===', error);
+    }
+  }
+
+  async function takeAndPostData(event, data) {
     event.preventDefault();
-    console.log('event.target.children ===', event.target.elements.email.value);
+    // console.log('event.target.children ===', event.target.elements.email.value);
 
     const formObj = {
       firstname: event.target.elements.firstName.value,
@@ -14,18 +34,29 @@ function Form() {
       subject: event.target.elements.subject.value,
       message: event.target.elements.message.value,
     };
-    console.log('formObj ===', formObj);
+    // console.log('formObj ===', formObj);
+    postForm(formObj);
+    event.target.reset();
+    // console.log('createdAtt ===', createdAtt);
   }
+
   return (
     <div className='form-container'>
-      <form onSubmit={logEvent}>
-        <InputField inputLabel='First Name' type='text' classDiv='name' name='firstName' />
-        <InputField inputLabel='Last Name' type='text' classDiv='name' name='lastName' />
-        <InputField inputLabel='Email' type='email' class='wide' name='email' />
-        <InputField inputLabel='Subject' type='text' class='wide' name='subject' />
-        <TextArea inputLabel='Message' class='wide' name='message' />
-        <Sbutton text='send message' type='submit' />
-      </form>
+      {createdAtt && (
+        <div className='success'>
+          <h3 className='success-msg'>Gavome jusu uzklausa, netrukus susisieksime</h3>
+        </div>
+      )}
+      {!createdAtt && (
+        <form onSubmit={takeAndPostData}>
+          <InputField inputLabel='First Name' type='text' classDiv='name' name='firstName' />
+          <InputField inputLabel='Last Name' type='text' classDiv='name' name='lastName' />
+          <InputField inputLabel='Email' type='email' class='wide' name='email' />
+          <InputField inputLabel='Subject' type='text' class='wide' name='subject' />
+          <TextArea inputLabel='Message' class='wide' name='message' />
+          <Sbutton text='send message' type='submit' />
+        </form>
+      )}
     </div>
   );
 }
